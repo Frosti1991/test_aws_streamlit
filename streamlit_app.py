3c4c88500283a11191cef14edd6f853fb7b3629f
Frosti1991 <82757760+Frosti1991@users.noreply.github.com>
2022-12-12 17:28:14 +0100

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#Tab Notation
+st.set_page_config(page_title="DayAhead Price Predictor", page_icon=":electric_plug:",layout="wide")
+
+with st.container():
+    st.title('Welcome to the Energy Market!')
+    st.header("Let's predict DayAhead Prices of European Power Exchange")
+
